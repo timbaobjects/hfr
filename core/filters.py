60a7691d994c46fc86a8c9af2f676b77bda60d96
Oladipo Odumosu <dodumosu@timbaobjects.com>
2014-07-22 06:00:23 +0100

    Added more filters

--- core/filters.py
+++ core/filters.py
@@ -2,9 +2,14 @@ from dateutil.parser import parse
 from django import forms
 import django_filters
 from rapidsms.contrib.messagelog.models import Message
+from locations.models import Location, LocationType
 
 
 def parse_date_range(value, sep='-'):
+    '''Given two string representations of datetime values with
+    a separator between them, attempts to parse for valid values.
+
+    Requires python-dateutil.'''
     try:
         start, end = [parse(p) for p in value.split(sep)]
     except Exception:
@@ -13,16 +18,33 @@ def parse_date_range(value, sep='-'):
     return start, end
 
 
-class MessageDateTimeRangeFilter(django_filters.CharFilter):
+class BaseDateTimeRangeFilter(django_filters.CharFilter):
+    '''Uses `parse_date_range` to filter a queryset for records
+    that have a property with the given `field_name` lying between
+    the parsed dates, or an empty queryset if there is a parse error.
+
+    Must be subclassed and `field_name` set to be useful, since
+    the default implementation just raised `ValueError`.'''
+    field_name = None
+
     def filter(self, qs, value):
+        if not self.field_name:
+            raise ValueError('Improperly configured: field_name must be set')
+
+        field_kwarg_key = '{}__range'.format(self.field_name)
+
         if value:
             res = parse_date_range(value)
             if not res:
-                return qs
+                return qs.none()
 
-            return qs.filter(date__range=res)
+            return qs.filter(**{field_kwarg_key: res})
 
         return qs
+
+
+class MessageDateTimeRangeFilter(BaseDateTimeRangeFilter):
+    field_name = 'date'
 
 
 class MessageDirectionFilter(django_filters.ChoiceFilter):
@@ -41,6 +63,57 @@ class MessageConnectionFilter(django_filters.CharFilter):
     def filter(self, qs, value):
         if value:
             return qs.filter(connection__identity__contains=value)
+
+        return qs
+
+
+class BaseLocationFilter(django_filters.ChoiceFilter):
+    def __init__(self, *args, **kwargs):
+        loc_type_qs = kwargs.pop('queryset', LocationType.objects.all())
+        location_types = loc_type_qs.values('pk', 'name')
+        locations = Location.objects.filter(
+            location_type__pk__in=[t['pk'] for t in location_types]
+        ).order_by('location_type', 'name').values(
+            'pk', 'location_type__name', 'name'
+        )
+
+        filter_locations = {}
+        for location in locations:
+            filter_locations.setdefault(
+                location['location_type__name'], []
+            ).append((location['pk'], location['name']))
+
+        kwargs['choices'] = [['', '']] + \
+            [[k, filter_locations[k]] for k in filter_locations.keys()]
+
+        return super(BaseLocationFilter, self).__init__(*args, **kwargs)
+
+    def filter(self, qs, value):
+        raise NotImplementedError('Please subclass and implement filter()')
+
+
+class ReportLocationFilter(BaseLocationFilter):
+    def filter(self, qs, value):
+        if value:
+            try:
+                location = Location.objects.get(pk=value)
+                return qs.filter_in(location)
+            except Location.DoesNotExist:
+                return qs.none()
+
+        return qs
+
+
+class WorkerLocationFilter(BaseLocationFilter):
+    def filter(self, qs, value):
+        if value:
+            try:
+                location = Location.objects.get(pk=value)
+                descendant_pks = [node['id']
+                                  for node in location.nx_descendants(True)]
+                return qs.filter(location__in=descendant_pks)
+            except Location.DoesNotExist:
+                return qs.none()
 
         return qs
 
